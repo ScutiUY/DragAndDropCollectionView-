7230e387c3b4331991c5296c0c78b72524942fdb
신의연 <dusdl14@gmail.com>
2022-05-20 09:25:05 +0900

    configure Existed cell and New cell moving function

--- DragAndDropCollectionView/ExistingCollectionViewController.swift
+++ DragAndDropCollectionView/ExistingCollectionViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    func setLayout() {
+        
+        
     }
 
 
 }
+class ViewController: UICollectionViewDragDelegate, UICollectionViewDropCoordinator
 
---
+++ DragAndDropCollectionView/View/CollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionViewCell.swift
+//  DragAndDropCollectionView
+//
+//  Created by 신의연 on 2022/05/17.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
