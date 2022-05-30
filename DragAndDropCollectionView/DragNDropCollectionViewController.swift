4b19182e6a6fca53f088bda6439171e8f8a6845e
신의연 <dusdl14@gmail.com>
2022-05-30 16:46:48 +0900

    configure DragNDropCellMoving

--- DragAndDropCollectionView/DragNDropCollectionViewController.swift
+++ DragAndDropCollectionView/DragNDropCollectionViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class NewCollectionViewController: UIViewController {
+class DragNDropCollectionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
