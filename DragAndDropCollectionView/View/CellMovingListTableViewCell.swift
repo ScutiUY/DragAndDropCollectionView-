cee63f28da4c2431394d230f45dfc8efe134fa55
신의연 <dusdl14@gmail.com>
2022-05-30 16:46:30 +0900

    Configure TableView Cell

--- DragAndDropCollectionView/View/CellMovingListTableViewCell.swift
+++ DragAndDropCollectionView/View/CellMovingListTableViewCell.swift
@@ -6,18 +6,26 @@
 //
 
 import UIKit
+import SnapKit
 
 class CellMovingListTableViewCell: UITableViewCell {
 
+    lazy var label: UILabel = {
+        var label = UILabel()
+        return label
+    }()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        self.addSubview(label)
+        
         // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
