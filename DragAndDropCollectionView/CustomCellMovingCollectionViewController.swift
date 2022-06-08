a4743894a8effce7499092923ae09f9a35b8354a
신의연 <dusdl14@gmail.com>
2022-06-08 16:54:34 +0900

    complete Custom Cell Moving

--- DragAndDropCollectionView/CustomCellMovingCollectionViewController.swift
+++ DragAndDropCollectionView/CustomCellMovingCollectionViewController.swift
@@ -8,131 +8,140 @@
 import UIKit
 
 class CustomCellMovingCollectionViewController: UIViewController {
-
-    var longPress: UILongPressGestureRecognizer!
     
+    var longPressGestureRecognizer: UILongPressGestureRecognizer!
+    
+    var originCell: UICollectionViewCell?
     var originalIndexPath: IndexPath?
     var draggingIndexPath: IndexPath?
     var draggingView: UIView?
     var dragOffset = CGPoint.zero
-    let color: [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple, .brown, .black, .gray, .cyan]
+    var color: [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple, .brown, .black, .gray, .cyan]
     
     lazy var collectionView: UICollectionView = {
-       let layout = UICollectionViewFlowLayout()
-       var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-       collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-       return collectionView
+        let layout = UICollectionViewFlowLayout()
+        var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setLayout()
-        // Do any additional setup after loading the view.
+        addGesture()
     }
     
     func setLayout(){
-    
+        
         view.addSubview(collectionView)
         
         collectionView.snp.makeConstraints { make in
-           make.top.equalTo(view.snp.top)
-           make.width.equalTo(view.snp.width)
-           make.centerX.equalToSuperview()
-           make.bottom.equalToSuperview()
+            make.top.equalTo(view.snp.top)
+            make.width.equalTo(view.snp.width)
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview()
         }
         
         collectionView.delegate = self
         collectionView.dataSource = self
-        
+        collectionView.dragInteractionEnabled = true
+    }
+    
+    func addGesture() {
+        longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
+        collectionView.addGestureRecognizer(longPressGestureRecognizer)
     }
     
     func startDragAtLocation(location: CGPoint) {
-       let cv = collectionView
-       guard let indexPath = cv.indexPathForItem(at: location) else { return }
-       guard cv.dataSource?.collectionView?(cv, canMoveItemAt: indexPath) == true else { return }
-       guard let cell = cv.cellForItem(at: indexPath) else { return }
-       print(cell)
-       
-    
-       originalIndexPath = indexPath
-       draggingIndexPath = indexPath
-       draggingView = cell.snapshotView(afterScreenUpdates: true)
-       draggingView!.frame = cell.frame
-       
-       cv.addSubview(draggingView!)
-       
-       dragOffset = CGPoint(x: draggingView!.center.x - location.x, y: draggingView!.center.y - location.y)
-       draggingView?.layer.shadowPath = UIBezierPath(rect: draggingView!.bounds).cgPath
-       draggingView?.layer.shadowColor = UIColor.black.cgColor
-       draggingView?.layer.shadowOpacity = 0.8
-       draggingView?.layer.shadowRadius = 10
-       
-       
-       collectionView.collectionViewLayout.invalidateLayout()
-       
- //      UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 0, options: [], animations: {
- //
- //         self.draggingView?.alpha = 0.95
- //         self.draggingView?.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
- //      }, completion: nil)
+        print("start")
+        
+        guard let indexPath = collectionView.indexPathForItem(at: location) else { return }
+        
+        guard collectionView.dataSource!.collectionView!(collectionView, canMoveItemAt: indexPath) == true else { return }
+        guard let cell = collectionView.cellForItem(at: indexPath) else { return }
+        
+        originCell = cell
+        originalIndexPath = indexPath
+        draggingIndexPath = indexPath
+        draggingView = cell.snapshotView(afterScreenUpdates: true)
+        draggingView!.frame = cell.frame
+        
+        collectionView.addSubview(draggingView!)
+        
+        dragOffset = CGPoint(x: cell.center.x - location.x, y: cell.center.y - location.y)
+        
+        cell.isHidden = true
+        
+        collectionView.collectionViewLayout.invalidateLayout()
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 0, options: [], animations: {
+            self.draggingView!.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            self.collectionView.collectionViewLayout.invalidateLayout()
+        })
+        UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0, options: [], animations: {
+            self.draggingView!.transform = CGAffineTransform.identity
+            self.collectionView.collectionViewLayout.invalidateLayout()
+        })
     }
     
     func updateDragAtLocation(location: CGPoint) {
-       guard let view = draggingView else { return }
-       let cv = collectionView
-       guard let indexPath = cv.indexPathForItem(at: location) else { return }
-       guard let cell = cv.cellForItem(at: indexPath) else { return }
-       
-       cell.center = CGPoint(x: location.x + dragOffset.x, y: location.y + dragOffset.y)
-       
-       if let newIndexPath = cv.indexPathForItem(at: location) {
-          cv.moveItem(at: draggingIndexPath!, to: newIndexPath)
-          draggingIndexPath = newIndexPath
-       }
+        
+        draggingView!.center = CGPoint(x: location.x + dragOffset.x, y: location.y + dragOffset.y)
+        if let newIndexPath = collectionView.indexPathForItem(at: location) {
+            collectionView.moveItem(at: draggingIndexPath!, to: newIndexPath)
+            draggingIndexPath = newIndexPath
+        }
+        
     }
     
     func endDragAtLocation(location: CGPoint) {
-       guard let dragView = draggingView else { return }
-       guard let indexPath = draggingIndexPath else { return }
-       let cv = collectionView
-       guard let datasource = cv.dataSource else { return }
+        
+        guard let dragView = draggingView else { return }
+        guard let indexPath = draggingIndexPath else { return }
+        let cv = collectionView
+        guard let datasource = cv.dataSource else { return }
+
        
-       let targetCenter = datasource.collectionView(cv, cellForItemAt: indexPath).center
-       
-       let shadowFade = CABasicAnimation(keyPath: "shadowOpacity")
-       shadowFade.fromValue = 0.8
-       shadowFade.toValue = 0
-       shadowFade.duration = 0.4
-       dragView.layer.add(shadowFade, forKey: "shadowFade")
-       
- //      UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 0, options: [], animations: {
- //         dragView.center = targetCenter
- //         dragView.transform = CGAffineTransform.identity
- //
- //      }) { [self] (completed) in
- //
- //         if indexPath != (self.originalIndexPath!) {
- //            datasource.collectionView?(cv, moveItemAt: self.originalIndexPath!, to: indexPath)
- //         }
- //
- //         dragView.removeFromSuperview()
- //         self.draggingIndexPath = nil
- //         self.draggingView = nil
- //         collectionView.collectionViewLayout.invalidateLayout()
- //      }
-       if indexPath != (self.originalIndexPath!) {
-          datasource.collectionView?(cv, moveItemAt: self.originalIndexPath!, to: indexPath)
-       }
-       
-       dragView.removeFromSuperview()
-       self.draggingIndexPath = nil
-       self.draggingView = nil
-       collectionView.collectionViewLayout.invalidateLayout()
-       
+        if indexPath != (self.originalIndexPath!) {
+            datasource.collectionView?(cv, moveItemAt: self.originalIndexPath!, to: indexPath)
+        }
+        if let newIndexPath = collectionView.indexPathForItem(at: location) {
+            let cell = collectionView.cellForItem(at: newIndexPath)
+            cell!.isHidden = false
+        }
+        
+        dragView.removeFromSuperview()
+        self.draggingIndexPath = nil
+        self.draggingView = nil
+        
+        collectionView.collectionViewLayout.invalidateLayout()
+        
+    }
+    
+    @objc func handleLongPress() {
+        
+        longPressGestureRecognizer.minimumPressDuration = 0.2
+        
+        let location = longPressGestureRecognizer.location(in: collectionView)
+        
+        switch longPressGestureRecognizer.state {
+        case .began:
+            if collectionView.indexPathForItem(at: location) != nil {
+                startDragAtLocation(location: location)
+            }
+        case .changed:
+            print("location in changed", location)
+            updateDragAtLocation(location: location)
+        case .ended:
+            endDragAtLocation(location: location)
+        default:
+            endDragAtLocation(location: location)
+        }
     }
 }
 
-extension CustomCellMovingCollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+extension CustomCellMovingCollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return color.count
     }
@@ -143,5 +152,12 @@ extension CustomCellMovingCollectionViewController: UICollectionViewDelegate, UI
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
     
+    func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
 }
+
