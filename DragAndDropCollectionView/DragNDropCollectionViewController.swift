e28975445c9b00c858fcde5dcb968b2f8c0b76f9
신의연 <dusdl14@gmail.com>
2022-06-08 16:54:48 +0900

    configure

--- DragAndDropCollectionView/DragNDropCollectionViewController.swift
+++ DragAndDropCollectionView/DragNDropCollectionViewController.swift
@@ -8,13 +8,63 @@
 import UIKit
 
 class DragNDropCollectionViewController: UIViewController {
-
+    
+    var longPress: UILongPressGestureRecognizer!
+    
+    let color: [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple, .brown, .black, .gray, .cyan]
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setLayout()
     }
     
-
-
+    func setLayout() {
+        
+        view.addSubview(collectionView)
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.top)
+            make.width.equalTo(view.snp.width)
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+        
+    }
+    
 }
+
+extension DragNDropCollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return color.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        cell.backgroundColor = color[indexPath.row]
+        return cell
+    }
+    
+    
+}
+
+//extension DragNDropCollectionViewController: UICollectionViewDragDelegate, UICollectionViewDropDelegate {
+//    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+//        let item = NSItemProvider(object: NSItemProviderWriting)
+//        return []
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+//        
+//    }
+//    
+//    
+//}
